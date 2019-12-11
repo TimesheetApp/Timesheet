f94f7d05e3ac96440691585bb5e0337d88cb4d75
Soheila Escobar <soheilaescobar@Soheilas-MacBook-Pro.local>
2019-12-11 14:35:07 -0500

    Hc

--- src/main/java/jbc/timesheet/controller/TimesheetController.java
+++ src/main/java/jbc/timesheet/controller/TimesheetController.java
@@ -1,20 +1,66 @@
 package jbc.timesheet.controller;
 
-import jbc.timesheet.service.EmailServiceImpl;
+
+import jbc.timesheet.model.Timesheet;
+import jbc.timesheet.repository.TimesheetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/timesheet")
-public class TimesheetController extends TimesheetControllerAbstract {
+public class TimesheetController  {
 
     @Autowired
-    EmailServiceImpl emailService;
+    TimesheetRepository timesheetRepository;
 
-
-    @Override
-    void doSomething() {
+    //Create (addTimesheet)
+    @GetMapping("/create")
+    public String createTimesheetform(Model model){
+        model.addAttribute("timesheet", new Timesheet());
+        return "timesheetform";
 
     }
-}
+
+    @PostMapping("/process")
+    public String processTimesheetform(@Valid @ModelAttribute Timesheet timesheet, BindingResult result){
+
+        if(result.hasErrors()){
+            return "timesheetform";
+        }
+
+        timesheetRepository.save(timesheet);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateTimesheetform(@PathVariable("id") Long id, Model model){
+        model.addAttribute("timesheet", timesheetRepository.findById(id).get());
+        return "timesheetform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteTimesheetform(@PathVariable("id") long id){
+        timesheetRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/view{id}")
+    public String viewTimesheetform(@PathVariable("id") Long id, Model model){
+        model.addAttribute("timesheet", timesheetRepository.findById(id).get());
+        return "timesheetform";
+    }
+
+    @GetMapping("/show")
+    public String viewTimesheetform(Model model){
+        model.addAttribute("timesheets", timesheetRepository.findAll());
+        return "listtimesheetform";
+    }
+
+
+    }
+
