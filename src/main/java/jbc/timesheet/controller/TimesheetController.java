7986e81d36d66acd53e8fc01779e5c736cf168bb
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-12-12 13:49:39 -0500

    Conf.Resolv

---
+++ src/main/java/jbc/timesheet/controller/TimesheetController.java
@@ -0,0 +1,40 @@
+
+package jbc.timesheet.controller;
+
+
+import jbc.timesheet.controller.iface.JediController;
+import jbc.timesheet.model.Timesheet;
+import jbc.timesheet.repository.TimesheetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/timesheet")
+public class TimesheetController implements JediController<TimesheetRepository, Timesheet, Long> {
+
+    @Autowired
+    TimesheetRepository timesheetRepository;
+
+    @Override
+    public Timesheet newEntity() {
+        return new Timesheet();
+    }
+
+    @Override
+    public TimesheetRepository getRepository() {
+        return timesheetRepository;
+    }
+
+    @Override
+    public String getTemplatePrefix() {
+        return "timesheet";
+    }
+
+    @Override
+    public Long getId(Timesheet timesheet) {
+        return timesheet.getId();
+    }
+
+
+}
